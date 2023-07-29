3c96e88c38addd0ac6b2e843c800472d5c1272ce
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-07-29 18:21:25 +0530

    added basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos excepturi maiores ea optio. Numquam hic, soluta nesciunt molestias mollitia magnam maiores, dicta, tempora iusto sint iure rem! Aliquid, impedit commodi!",
+            price
         })
         await camp.save();
     }
