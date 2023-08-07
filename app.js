f5076a227de1cb2015bc75b0311ab844ec6f5a2a
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-08-07 23:01:15 +0530

    Reviews Added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const Campground = require('./models/campground');
 const ExpressError = require('./utils/ExpressError')
 const catchAsync = require('./utils/catchAsync');
+const Review = require('./models/review')
 const { campgroundSchema } = require('./schema.js');
 const methodOverride = require('method-override');
 
@@ -72,6 +73,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const deletedproduct = await Campground.findByIdAndDelete( id );
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync( async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next) => {
