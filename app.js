720af71fc139323cabdd87e13507da130bd43552
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-08-18 14:09:00 +0530

    Session and Cookie Included

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const path = require('path');
 const Campground = require('./models/campground');
 const ExpressError = require('./utils/ExpressError')
@@ -24,6 +25,20 @@ db.once("open", () => {
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret : 'thismustbeagoodsecret',
+    resave : false,
+    saveUninitialized: true,
+    cookie : {
+        httpOnly : true,
+        expires : Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge : 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
