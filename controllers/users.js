a4c70e60160bdeb95d768a0528a3959d4babeaf2
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-09-10 13:39:29 +0530

    ´Fixed Register Route

--- controllers/users.js
+++ controllers/users.js
@@ -9,8 +9,11 @@ module.exports.register = async (req, res) => {
         const {username, email, password} = req.body;
         const user = new User({ username, email })
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to CampHub!')
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to CampHub!')
+            res.redirect('/campgrounds');
+        })
     }
     catch(e){
         req.flash('error', e.message);
