b86dcc26f9b111d64df21fdbc20ad35fa280a7b9
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-09-15 17:21:46 +0530

    Added title to the marker

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
     title : String,
@@ -34,7 +35,12 @@ const CampgroundSchema = new Schema({
             ref : 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+    return `<a href="/campgrounds/${this._id}"> ${this.title} </a>`
 })
+
 
 CampgroundSchema.post('findOneAndDelete', async function(doc) {
     if(doc){
--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -103,10 +103,8 @@ zoom: zoom
 // the location of the feature, with
 // description HTML from its properties.
 map.on('click', 'unclustered-point', (e) => {
-const coordinates = e.features[0].geometry.coordinates.slice();
-const mag = e.features[0].properties.mag;
-const tsunami =
-e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+    const text = e.features[0].properties.popUpMarkup;
+    const coordinates = e.features[0].geometry.coordinates.slice();
  
 // Ensure that if the map is zoomed out such that
 // multiple copies of the feature are visible, the
@@ -118,7 +116,7 @@ coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 new mapboxgl.Popup()
 .setLngLat(coordinates)
 .setHTML(
-`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
+   text
 )
 .addTo(map);
 });
