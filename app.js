d5c950bf93a141a0b0c22e72d7167ebe8c7ac0eb
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-09-20 18:28:43 +0530

    Added MongoURL

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const dbUrl = process.env.DB_URL;
+
+//mongodb://127.0.0.1:27017/yelp-camp
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
